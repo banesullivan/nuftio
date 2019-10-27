5ddd5bfa4bc661d7e260d6b63000b41a75c933f4
Bane Sullivan <banesullivan@gmail.com>
2019-10-26 18:19:13 -0600

    Update __init__.py

--- nuftio/__init__.py
+++ nuftio/__init__.py
@@ -7,7 +7,7 @@ from .spec import *
 # Package meta data
 __author__ = 'Bane Sullivan'
 __license__ = 'MIT'
-__copyright__ = '2018, Bane Sullivan'
+__copyright__ = '2019, Bane Sullivan'
 __version__ = '0.0.0'
 __displayname__ = 'nuftio'
 __name__ = 'nuftio'
